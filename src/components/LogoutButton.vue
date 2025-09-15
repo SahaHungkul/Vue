9076f127d6a931a57b70cab8c6a34bbbd6bc3a09
SahaHungkul <mhasbysyabani27@gmail.com>
2025-09-15 16:13:21 +0700

    feat add LogoutButton Component

---
+++ src/components/LogoutButton.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { useUserStore } from '@/stores/user';
+import { Button } from '@/components/ui/button';
+
+const router = useRoute();
+const userStore = useUserStore();
+
+function handleLogout() {
+  userStore.logout();
+  router.push('/login');
+}
+</script>
+
+<template>
+  <Button @click="handleLogout" variant="destructive" class="w-full">Logout</Button>
+</template>
+
+<style scoped></style>
